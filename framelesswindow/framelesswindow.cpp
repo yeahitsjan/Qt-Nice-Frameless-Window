330c4171ce0030275171604d33dade741fa9ae0f
Jan <74990847+yeahitsjan@users.noreply.github.com>
2021-08-29 22:32:01 +0200

    Fix GdiPlusColor missing with mingw

--- framelesswindow/framelesswindow.cpp
+++ framelesswindow/framelesswindow.cpp
@@ -10,7 +10,8 @@
 #include <dwmapi.h>
 #include <objidl.h> // Fixes error C2504: 'IUnknown' : base class undefined
 #include <gdiplus.h>
-#include <GdiPlusColor.h>
+//#include <GdiPlusColor.h>
+#include <gdiplus/gdipluscolor.h> // Fixes missing GdiPlusColor.h on mingw
 #pragma comment (lib,"Dwmapi.lib") // Adds missing library, fixes error LNK2019: unresolved external symbol __imp__DwmExtendFrameIntoClientArea
 #pragma comment (lib,"user32.lib")
 
